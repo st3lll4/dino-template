98c5a30db6b97f27068cdd32072106bd16ead4bb
st3lll4 <stella_tukia@hotmail.com>
2026-03-26 20:55:25 +0200

    remove todo

--- src/api/types.ts
+++ src/api/types.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 export type HttpMethod = "GET" | "POST" | "PUT" | "DELETE";
 
-// TODO: add typed params validation using Zod
 export type EndpointDefinition<TBody extends z.ZodTypeAny = z.ZodTypeAny> = {
   url: string;
   method: HttpMethod;
